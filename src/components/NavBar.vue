7858734c62afe8d8a6c18f1c24a3de253560fc97
Arimos <amilinkovic.ns@gmail.com>
2018-09-17 19:24:51 +0200

    prvi commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+    <nav class="navbar navbar-light bg-light">
+        <router-link to="/">All Galleries</router-link>
+    </nav>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  
+}
+</script>
+
+
